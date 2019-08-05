311a69510865b413c86f4d07fe6f7c5168845436
Omkar Kusre <Omkar.Kusre@ibm.com>
2019-08-05 18:39:08 +0530

    Initial Package Structure

---
+++ testproject/src/main/java/com/rackspace/hr/management/controller/impl/LeaveDataControllerImpl.java
@@ -0,0 +1,42 @@
+package com.rackspace.hr.management.controller.impl;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import com.rackspace.hr.management.controller.LeaveDataController;
+import com.rackspace.hr.management.dto.LeaveDataDTO;
+
+public class LeaveDataControllerImpl implements LeaveDataController{
+
+    @Override
+    public LeaveDataDTO createLeaveData(@Valid LeaveDataDTO body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<LeaveDataDTO> getAllLeaveData() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public LeaveDataDTO getLeaveDataById() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public LeaveDataDTO updateLeaveData(String leaveDataId, @Valid LeaveDataDTO body) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String deleteLeaveData() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
