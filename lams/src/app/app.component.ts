39f783ab2edfdd401cb512d3dc9fe3506bb0e9c9
prabhakar <prabhakar2020@gmail.com>
2019-08-05 15:53:14 +0530

    added basic lams angular application

---
+++ lams/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Leave & Attendance Management System';
+  footer= 'Design By Prabhakar';
+}
